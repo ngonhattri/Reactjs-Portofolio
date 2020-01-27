407f12c66625cdf6e6305098f4022e2e3fcdf8ed
minhquan3197 <minhquan.k0r1@gmail.com>
2020-01-27 16:49:38 +0700

    React router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, HashRouter, Switch } from 'react-router-dom';
+import routes from './routes';
+import Footer from './components/layouts/Footer';
+import Header from './components/layouts/Header';
+import Sidebar from './components/layouts/Sidebar';
+const App = () => {
+    const showContent = routes => {
+        var result = null;
+        if (routes.length > 0) {
+            result = routes.map((route, index) => {
+                return (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.main}
+                        layout={route.layout}
+                    />
+                );
+            });
+        }
+        return <Switch>{result}</Switch>;
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter>
+            {showContent(routes)}
+            <Header />
+            <Sidebar />
+            <Footer />
+        </HashRouter>
+    );
 }
+
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import Home from './components/home/Home';
+
+const routes = [
+    {
+        path: "/",
+        exact: true,
+        main: Home,
+    },
+];
+
+export default routes;
