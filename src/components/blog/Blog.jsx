754f1bd7f91641559d3c95fd103d5ca128074faa
minhquan3197 <minhquan.k0r1@gmail.com>
2020-01-29 23:30:43 +0700

    Reszie blog

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Typography, Row, Col, Card } from 'antd';
+import { Link } from 'react-router-dom';
+const { Title } = Typography;
+const { Meta } = Card;
 
 const Blog = () => {
     return (
         <>
-            <h1>Hello</h1>
+            <Title level={2}>Quan Nguyen Blog</Title>
+            <Row>
+                <Col
+                    xs={24}
+                    sm={24}
+                    md={6}
+                    lg={6}
+                    xl={6}
+                    style={{ paddingRight: 10, paddingBottom: 10 }}
+                >
+                    <Link to="/">
+                        <Card
+                            className="blog-card"
+                            cover={
+                                <img
+                                    alt="example"
+                                    src="https://res.cloudinary.com/kori/image/upload/v1564762054/blogs/code.jpg"
+                                />
+                            }
+                            style={{ height: 345 }}
+                        >
+                            <Meta title="Test" />
+                        </Card>
+                    </Link>
+                </Col>
+            </Row>
         </>
     );
 };
